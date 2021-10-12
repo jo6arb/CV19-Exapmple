486e42382771d3fa132f0ab46e8e9904f8309e32
aleksey pavlov <alekseyelit1337@gmail.com>
2021-10-12 20:13:02 +0300

    Создали базовый класс модели-представления

---
+++ CV19/ViewModels/Base/ViewModel.cs
@@ -0,0 +1,25 @@
+﻿using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using CV19.Annotations;
+
+namespace CV19.ViewModels.Base
+{
+    internal abstract class ViewModel : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        [NotifyPropertyChangedInvocator]
+        protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+        protected virtual bool Set<T>(ref T field, T value, [CallerMemberName] string PropertyName = null)
+        {
+            if (Equals(field,value)) return false;
+            field = value;
+            OnPropertyChanged(PropertyName);
+            return true;
+        }
+    }
+}
