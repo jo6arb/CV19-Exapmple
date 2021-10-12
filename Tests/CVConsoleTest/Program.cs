cc05270f35d94ad1e5bf8c1594bc2a25f09aa836
aleksey pavlov <alekseyelit1337@gmail.com>
2021-10-12 11:15:15 +0300

    Добавили тестовые проекты

---
+++ Tests/CVConsoleTest/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CVConsoleTest
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
