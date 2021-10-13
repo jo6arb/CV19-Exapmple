312c9882b68c977878ac0bd1681e17ddf083b7b3
aleksey pavlov <alekseyelit1337@gmail.com>
2021-10-13 10:51:37 +0300

    Главный класс команд для приложения

---
+++ CV19/Infrastructure/Commands/LambdaCommand.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using CV19.Infrastructure.Commands.Base;
+
+namespace CV19.Infrastructure.Commands
+{
+    public class LambdaCommand : Command
+    {
+        private readonly Action<object> _execute;
+        private readonly Func<object, bool> _canExecute;
+
+        public LambdaCommand(Action<object> Execute, Func<Object, bool> CanExecute = null)
+        {
+            _execute = Execute ?? throw new ArgumentNullException(nameof(Execute));
+            _canExecute = CanExecute;
+        }
+
+        public override bool CanExecute(object parameter) => _canExecute?.Invoke(parameter) ?? true;
+
+        public override void Execute(object parameter) => _execute(parameter);
+    }
+}
