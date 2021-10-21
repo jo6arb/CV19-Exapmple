fd4261fc9ac3d26b0c1c5bb1c85e53b7c5c8cc5f
aleksey pavlov <alekseyelit1337@gmail.com>
2021-10-21 21:13:03 +0300

    Предметная область приложения

---
+++ CV19/Models/PlaceInfo.cs
@@ -0,0 +1,11 @@
+﻿using System.Windows;
+
+namespace CV19.Models
+{
+    internal class PlaceInfo
+    {
+        public string Name { get; set; }
+
+        public Point Location { get; set; }
+    }
+}
