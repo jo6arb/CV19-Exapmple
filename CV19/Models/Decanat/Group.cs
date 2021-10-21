323927a362228277256f577398fe9f804cbe5ef5
aleksey pavlov <alekseyelit1337@gmail.com>
2021-10-21 21:12:51 +0300

    Тестовая предметтная област

---
+++ CV19/Models/Decanat/Group.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+
+namespace CV19.Models.Decanat
+{
+    internal class Group
+    {
+        public string Name { get; set; }
+
+        public ICollection<Student> Students { get; set; }
+    }
+}
---
+++ CV19/Models/Decanat/Student.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace CV19.Models.Decanat
+{
+    internal  class Student
+    {
+        public string Name { get; set; }
+
+        public string Surname { get; set; }
+
+        public string Patronymic { get; set; }
+
+        public DateTime Birthday { get; set; }
+
+        public double Rating { get; set; }
+    }
+}
