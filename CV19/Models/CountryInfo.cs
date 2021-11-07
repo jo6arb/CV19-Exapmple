2f8a4fad8c79bbf786f50d602e528026fe6b2748
aleksey pavlov <alekseyelit1337@gmail.com>
2021-11-07 15:51:15 +0300

    Изменили модель для работы с графиком

--- CV19/Models/CountryInfo.cs
+++ CV19/Models/CountryInfo.cs
@@ -25,5 +25,33 @@ namespace CV19.Models
         }
 
         public IEnumerable<PlaceInfo> ProvinceCounts { get; set; }
+
+        private ConfirmedCount[] _counts;
+        private IEnumerable<ConfirmedCount> _counts1;
+
+        public override IEnumerable<ConfirmedCount> Counts
+        {
+            get
+            {
+                if (_counts != null) return _counts;
+                var pointsCount = ProvinceCounts.FirstOrDefault()?.Counts?.Count() ?? 0;
+                if (pointsCount == 0) return Enumerable.Empty<ConfirmedCount>();
+
+                var provincePoints = ProvinceCounts.Select(p => p.Counts.ToArray()).ToArray();
+
+                var points = new ConfirmedCount[pointsCount];
+                foreach (var province in provincePoints)
+                    for (var i = 0; i < pointsCount; i++)
+                    {
+                        if (points[i].Date == default)
+                            points[i] = province[i];
+                        else
+                            points[i].Count += province[i].Count;
+                    }
+
+                return _counts = points;
+            }
+            set => _counts1 = value;
+        }
     }
 }
