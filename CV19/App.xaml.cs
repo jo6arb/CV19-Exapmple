aea2a53e2ec6d1237376e1034a3676b50ca9ff0b
aleksey pavlov <alekseyelit1337@gmail.com>
2021-11-08 12:54:49 +0300

    Инфраструктура для создания хоста приложения

--- CV19/App.xaml.cs
+++ CV19/App.xaml.cs
@@ -1,11 +1,21 @@
 ﻿using System.Linq;
 using System.Windows;
 using CV19.Services;
+using CV19.ViewModels;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
 
 namespace CV19
 {
 
     public partial class App : Application
     {
+        
+        public static void ConfigureServices(HostBuilderContext host, IServiceCollection services)
+        {
+            services.AddSingleton<DataService>();
+            services.AddSingleton<CountryStatisticViewModel>();
+        }
     }
+    
 }
--- CV19/Program.cs
+++ CV19/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Hosting;
 
 namespace CV19
 {
@@ -10,6 +12,22 @@ namespace CV19
             var app = new App();
             app.InitializeComponent();
             app.Run();
+        }
+
+        public static IHostBuilder CreateHostBuilder(string[] Agrs)
+        {
+            var hostBuilder = Host.CreateDefaultBuilder(Agrs);
+
+            hostBuilder.UseContentRoot(Environment.CurrentDirectory);
+            hostBuilder.ConfigureAppConfiguration((host, cfg) =>
+            {
+                cfg.SetBasePath(Environment.CurrentDirectory);
+                cfg.AddJsonFile("appSettings.json", optional: true, reloadOnChange: true);
+            });
+
+            hostBuilder.ConfigureServices(App.ConfigureServices);
+
+            return hostBuilder;
         }
     }
 }
