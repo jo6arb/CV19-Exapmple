a93f2695931fe65d1a8500c2aebbfe8633fd9b61
aleksey pavlov <alekseyelit1337@gmail.com>
2021-11-08 13:01:27 +0300

    Запуск и остановка хоста при выходе

--- CV19/App.xaml.cs
+++ CV19/App.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System.Linq;
+﻿using System;
+using System.Linq;
 using System.Windows;
 using CV19.Services;
 using CV19.ViewModels;
@@ -10,7 +11,29 @@ namespace CV19
 
     public partial class App : Application
     {
-        
+
+        private static IHost _host;
+
+        public static IHost Host => _host ??= Program.CreateHostBuilder(Environment.GetCommandLineArgs()).Build();
+
+        protected override async void OnStartup(StartupEventArgs e)
+        {
+            base.OnStartup(e);
+            var host = Host;
+
+            await host.StartAsync().ConfigureAwait(false);
+        }
+
+        protected override  async void OnExit(ExitEventArgs e)
+        {
+            base.OnExit(e);
+            var host = Host;
+
+            await Host.StopAsync().ConfigureAwait(false);
+            host.Dispose();
+            _host = null;
+        }
+
         public static void ConfigureServices(HostBuilderContext host, IServiceCollection services)
         {
             services.AddSingleton<DataService>();
