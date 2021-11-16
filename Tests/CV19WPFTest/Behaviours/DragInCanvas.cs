4103c828b46aa3c6ca73817ac600ff6658d96853
aleksey pavlov <alekseyelit1337@gmail.com>
2021-11-16 15:02:41 +0300

    Повередие, способное реагировать на события мыши

---
+++ Tests/CV19WPFTest/Behaviours/DragInCanvas.cs
@@ -0,0 +1,58 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Diagnostics;
+using System.Windows.Input;
+using System.Windows.Media;
+using Microsoft.Xaml.Behaviors;
+
+namespace CV19WPFTest.Behaviours
+{
+    public class DragInCanvas : Behavior<UIElement>
+    {
+        private Point _startPoint;
+        private Canvas _canvas;
+        protected override void OnAttached()
+        {
+            AssociatedObject.MouseLeftButtonDown += OnButtonDown;
+
+        }
+        
+        protected override void OnDetaching()
+        {
+            AssociatedObject.MouseLeftButtonDown -= OnButtonDown;
+            AssociatedObject.MouseMove -= OnMouseMove;
+            AssociatedObject.MouseUp -= OnMouseUp;
+        }
+
+        private void OnButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            if((_canvas ??= VisualTreeHelper.GetParent(AssociatedObject) as Canvas) is null)
+                return;
+            
+
+            _startPoint = e.GetPosition(AssociatedObject);
+            AssociatedObject.CaptureMouse();
+            AssociatedObject.MouseMove += OnMouseMove;
+            AssociatedObject.MouseUp += OnMouseUp;
+        }
+
+        private void OnMouseUp(object sender, MouseButtonEventArgs e)
+        {
+            AssociatedObject.MouseMove -= OnMouseMove;
+            AssociatedObject.MouseUp -= OnMouseUp;
+            AssociatedObject.ReleaseMouseCapture();
+        }
+
+        private void OnMouseMove(object sender, MouseEventArgs e)
+        {
+            
+            var obj = AssociatedObject;
+            var currLoc = e.GetPosition(_canvas);
+
+            var delta = currLoc - _startPoint;
+            obj.SetValue(Canvas.LeftProperty, delta.X);
+            obj.SetValue(Canvas.TopProperty, delta.Y);
+
+        }
+    }
+}
