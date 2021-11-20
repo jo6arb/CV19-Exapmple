b24e6f623e0ac0ce22d5be267258a1ea265dbac4
aleksey pavlov <alekseyelit1337@gmail.com>
2021-11-20 12:29:49 +0300

    Добавление тестового окна для отработки стиля

---
+++ CV19/Views/Windows/TestWindow.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows;
+
+namespace CV19
+{
+    /// <summary>
+    /// Interaction logic for TestWindow.xaml
+    /// </summary>
+    public partial class TestWindow : Window
+    {
+        public TestWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
