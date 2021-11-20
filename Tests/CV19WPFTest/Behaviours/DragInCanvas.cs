4655018b48c224cc3330b039d2994f677ad50444
aleksey pavlov <alekseyelit1337@gmail.com>
2021-11-20 12:17:43 +0300

    Поведение для кнопки - закрытия окна + свойства-зависимости в поведениях

--- Tests/CV19WPFTest/Behaviours/DragInCanvas.cs
+++ Tests/CV19WPFTest/Behaviours/DragInCanvas.cs
@@ -1,4 +1,5 @@
-﻿using System.Windows;
+﻿using System.ComponentModel;
+using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Input;
 using System.Windows.Media;
@@ -10,6 +11,50 @@ namespace CV19WPFTest.Behaviours
     {
         private Point _startPoint;
         private Canvas _canvas;
+
+        #region PositionX : double - Горизонтальное смещение
+
+        ///<summary>Горизонтальное смещение</summary>
+        public static readonly DependencyProperty PositionXProperty =
+            DependencyProperty.Register(
+                nameof(PositionX),
+                typeof(double),
+                typeof(DragInCanvas),
+                new PropertyMetadata(default(double)));
+
+        ///<summary>Горизонтальное смещение</summary>
+        // [Category("")]
+        [Description("Горизонтальное смещение")]
+        public double PositionX
+        {
+            get => (double) GetValue(PositionXProperty);
+            set => SetValue(PositionXProperty, value);
+        }
+
+        #endregion
+
+        #region PositionY : double - Вертикальное смещение
+
+        ///<summary>Вертикальное смещение</summary>
+        public static readonly DependencyProperty PositionYProperty =
+            DependencyProperty.Register(
+                nameof(PositionY),
+                typeof(double),
+                typeof(DragInCanvas),
+                new PropertyMetadata(default(double)));
+
+        ///<summary>Вертикальное смещение</summary>
+        // [Category("")]
+        [Description("Вертикальное смещение")]
+        public double PositionY
+        {
+            get => (double) GetValue(PositionYProperty);
+            set => SetValue(PositionYProperty, value);
+        }
+
+        #endregion 
+
+   
         protected override void OnAttached()
         {
             AssociatedObject.MouseLeftButtonDown += OnButtonDown;
@@ -51,6 +96,9 @@ namespace CV19WPFTest.Behaviours
             var delta = currLoc - _startPoint;
             obj.SetValue(Canvas.LeftProperty, delta.X);
             obj.SetValue(Canvas.TopProperty, delta.Y);
+
+            PositionX = delta.X;
+            PositionY = delta.Y;
 
         }
     }
