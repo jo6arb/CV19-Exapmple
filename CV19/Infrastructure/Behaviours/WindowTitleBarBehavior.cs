9d740cc6cfc5d17d068a338feccbec920fd6a3b3
aleksey pavlov <alekseyelit1337@gmail.com>
2021-12-01 12:31:41 +0300

    Развертывание окна на весь экран по двойному щелчку и обратно

--- CV19/Infrastructure/Behaviours/WindowTitleBarBehavior.cs
+++ CV19/Infrastructure/Behaviours/WindowTitleBarBehavior.cs
@@ -17,10 +17,34 @@ namespace CV19.Infrastructure.Behaviours
         protected override void OnDetaching() => AssociatedObject.MouseLeftButtonDown -= OnMouseDown;
 
         private void OnMouseDown(object sender, MouseButtonEventArgs e)
-        { 
-           if(e.ClickCount > 1) return;
-           if (!(AssociatedObject.FindVisualRoot() is Window window)) return;
-           window.DragMove();
+        {
+            switch (e.ClickCount)
+            {
+                case 1:
+                    DragMode();
+                    break;
+                case 2:
+                    Maximize();
+                    break;
+            }
+           
+        }
+
+        private void DragMode()
+        {
+            if (!(AssociatedObject.FindVisualRoot() is Window window)) return;
+            window.DragMove();
+        }
+
+        private void Maximize()
+        {
+            if (!(AssociatedObject.FindVisualRoot() is Window window)) return;
+            window.WindowState = window.WindowState switch
+            {
+                WindowState.Normal => WindowState.Maximized,
+                WindowState.Maximized => WindowState.Normal,
+                _ => window.WindowState
+            };
         }
 
         
