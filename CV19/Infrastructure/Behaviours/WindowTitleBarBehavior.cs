d8d4788721552190bed5943cbdfc67e9e57b8181
aleksey pavlov <alekseyelit1337@gmail.com>
2021-12-01 12:21:14 +0300

    Методы-расширения для поиска элемента среди родительских в визуальном или логическом деревьях

---
+++ CV19/Infrastructure/Behaviours/WindowTitleBarBehavior.cs
@@ -0,0 +1,20 @@
+﻿using System.Security.Permissions;
+using System.Windows;
+using Microsoft.Xaml.Behaviors;
+
+namespace CV19.Infrastructure.Behaviours
+{
+    class WindowTitleBarBehavior : Behavior<UIElement>
+    {
+        private Window _window;
+        protected override void OnAttached()
+        {
+            _window = AssociatedObject as Window ?? AssociatedObject
+        }
+
+        protected override void OnDetaching()
+        {
+
+        }
+    }
+}
